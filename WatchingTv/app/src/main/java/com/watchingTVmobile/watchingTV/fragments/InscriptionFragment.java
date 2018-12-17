5ee9df9e46f4c91a81a573d566753be7189f4836
Wolfalfrack <nicolas.verhaeghe14@gmail.com>
2018-12-17 15:23:40 +0100

    changemenet editTExt

--- WatchingTv/app/src/main/java/com/watchingTVmobile/watchingTV/fragments/InscriptionFragment.java
+++ WatchingTv/app/src/main/java/com/watchingTVmobile/watchingTV/fragments/InscriptionFragment.java
@@ -5,21 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.watchingTVmobile.watchingTV.R;
 
 
 public class InscriptionFragment extends Fragment {
-    private TextView textViewUsername;
-    private TextView textViewPassword;
-    private TextView textViewPasswordconf;
+    private EditText edit_textViewUsername;
+    private EditText edit_textViewPassword;
+    private EditText edit_textViewPasswordconf;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inscription,container,false);
-        textViewUsername =(TextView) view.findViewById(R.id.edit_text_user);
-        textViewPassword =(TextView) view.findViewById(R.id.edit_text_password);
-        textViewPasswordconf =(TextView) view.findViewById(R.id.edit_text_conf);
+        edit_textViewUsername =(EditText) view.findViewById(R.id.edit_text_user);
+        edit_textViewPassword =(EditText) view.findViewById(R.id.edit_text_password);
+        edit_textViewPasswordconf =(EditText) view.findViewById(R.id.edit_text_conf);
         return view;
     }
 }
